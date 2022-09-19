5b89d29dc13ba6ab31586596ab73ca3ef2177261
JAgrit20 <jagritacharya2020@gmail.com>
2022-09-19 23:41:02 +0530

    Updates

---
+++ precache-manifest.2618847d487b80d32fd394a23bab563e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1140273e7f1674547f4fcb8bdd4efeb",
+    "url": "/Notes-Keeper/index.html"
+  },
+  {
+    "revision": "4ebd352b2f64db882d17",
+    "url": "/Notes-Keeper/static/js/2.423f9f20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Notes-Keeper/static/js/2.423f9f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef62a03470b1e6b2a76a",
+    "url": "/Notes-Keeper/static/js/main.84e5bba1.chunk.js"
+  },
+  {
+    "revision": "336de02262e1ec7d4d52",
+    "url": "/Notes-Keeper/static/js/runtime-main.f352adb3.js"
+  }
+]);
